20211e65534fe3dbe9a71429b80c9b183e1f6858
Yasmine BENDJEBBOUR <yasminebendjebbour@gmail.com>
2026-03-08 17:33:11 +0100

    Test de modele

--- src/main/java/future/SAE/domain/model/Eleve.java
+++ src/main/java/future/SAE/domain/model/Eleve.java
@@ -7,26 +7,24 @@ public class Eleve extends Utilisateur
     private Formation formation;
 
     // constructeur
-    public Eleve(String unNom, String unPrenom, int identifiant, String unEmail, String unMdp)
-    {
+    public Eleve(String unNom, String unPrenom, int identifiant, String unEmail, String unMdp) {
         super(unNom, unPrenom, identifiant, unEmail, unMdp, Role.ELEVE);
     }
 
     // ajouter constructeur avec l'id de la formation
-    public Eleve(String unNom, String unPrenom, int identifiant, String unMdp, String unEmail, Formation uneFormation)
-    {
+    public Eleve(String unNom, String unPrenom, int identifiant, String unMdp, String unEmail, Formation uneFormation) {
         super(unNom, unPrenom, identifiant, unEmail, unMdp, Role.ELEVE);
         this.formation = uneFormation;
     }
 
-    public Formation getIdFormation()
-    {
+    public Formation getIdFormation() {
         return this.formation;
     }
 
-    public void setIdFormation(Formation uneFormation)
-    {
+    public void setIdFormation(Formation uneFormation) {
         this.formation = uneFormation;
     }
+
+}
 
 
