fc10ac35eafbdf8d4b8107c28494591de4b3bf5f
Yasmine BENDJEBBOUR <yasminebendjebbour@gmail.com>
2026-03-08 21:18:45 +0100

    DTO

--- src/main/java/future/SAE/api/dto/UtilisateurReponseDTO.java
+++ src/main/java/future/SAE/api/dto/UtilisateurReponseDTO.java
@@ -1,6 +1,23 @@
 package future.SAE.api.dto;
 
+
 public class UtilisateurReponseDTO
 {
+    import java.time.LocalDateTime;
+import java.util.UUID;
 
+    @Getter
+    @Setter
+    public class UtilisateurResponseDTO {
+
+        private UUID id;
+        private int identifiant;
+        private String nom;
+        private String prenom;
+        private String email;
+        private Role role;
+        private UUID formationId;
+        private LocalDateTime dateCreation;
+
+    }
 }
