815ebb3a973a293d1b0c179acf345655a4547345
Yasmine BENDJEBBOUR <yasminebendjebbour@gmail.com>
2026-03-08 17:23:00 +0100

    Test de modele

--- src/main/java/future/SAE/domain/model/Professeur.java
+++ src/main/java/future/SAE/domain/model/Professeur.java
@@ -1,42 +1,75 @@
 package future.SAE.domain.model;
 import java.util.ArrayList;
-
+import java.util.List;
+// import java.util.ArrayList;
+// import future.SAE.domain.valueObject.InscriptionCours;
+import future.SAE.domain.valueObject.Role;
+// pas besoin de lier les eleves au prof
+// pas de methodes inscriptionEleve pour le moment
 public class Professeur extends Utilisateur
 {
-    private Eleve eleve;
-    private ArrayList <Cours> cours;
+    private List<Cours> coursDispenses = new ArrayList<>();
 
-    public Professeur(String unNom, String unPrenom, int id, String unEmail)
+    public Professeur(String unNom, String unPrenom, int identifiant, String unEmail, String unMdp)
     {
-        super(unNom, unPrenom, id, unEmail);
+        super(unNom, unPrenom, identifiant, unEmail, unMdp, Role.PROFESSEUR);
     }
 
-    public boolean inscriptionEleve(Eleve eleve)
-    {
-        if (this.cours.add(eleve))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    // public boolean inscriptionEleve(Eleve unEleve, Cours unCours) {
+    // if (unEleve != null && unCours != null) {
+    // InscriptionCours inscription = new InscriptionCours(unCours, unEleve);
+    // return true;
+    // } else {
+    // return false;
+    // }
+    // // if (this.cours.add(eleve))
+    // // {
+    // // return true;
+    // // }
+    // // else
+    // // {
+    // // return false;
+    // // }
+    // }
+
+    // public boolean desinscriptionEleve(Eleve eleve, Cours unCours)
+    // {
+    // // if (this.cours.remove(eleve))
+    // // {
+    // // return true;
+    // // }
+    // // else
+    // // {
+    // // return false;
+    // // }
+    // }
+
+    public List<Cours> getCours() {
+        return this.coursDispenses;
     }
 
-    public boolean desinscriptionEleve(Eleve eleve)
+    public void ajouterCours(Cours unCours)
     {
-        if (this.cours.remove(eleve))
+        if (unCours != null)
         {
-            return true;
-        }
-        else
-        {
-            return false;
+            this.coursDispenses.add(unCours);
         }
     }
+    // public ArrayList<Formation> getFormations()
+    // {
+    //      return this.formations;
+    // }
+
+    // public void ajouterFormation(Formation formation)
+    // {
+    //      if (formation != null)
+    //      {
+    //          this.formations.add(formation);
+    //      }
+    // }
 
     public String toString()
     {
-        return "Professeur" +super.toString();
+        return "Professeur" + super.toString();
     }
 }
