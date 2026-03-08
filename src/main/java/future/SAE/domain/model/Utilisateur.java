6d37d95a6c77f5fb6dbafb6038a3efe08e8149a4
Yasmine BENDJEBBOUR <yasminebendjebbour@gmail.com>
2026-03-08 17:13:22 +0100

    Test de modele

--- src/main/java/future/SAE/domain/model/Utilisateur.java
+++ src/main/java/future/SAE/domain/model/Utilisateur.java
@@ -1,30 +1,52 @@
 package future.SAE.domain.model;
-import java.util.Locale;
+import java.time.LocalDateTime;
+//import java.util.Locale;
+import java.util.UUID;
+import future.SAE.domain.valueObject.Role;
 
+// ajouter un uuid pour l'identifiant de l'utilisateur ?
+//ajouter un champs date de création du compte ?
+// ajouter un role pour différencier les élèves des professeurs ?
+// ajouter un 2eme constructeur
 public class Utilisateur
 {
+    private UUID id;
+    private int identifiant;
     private String nom;
     private String prenom;
-    private int identifiant;
     private String email;
     private String motDePasse;
+    private Role role;
+    private LocalDateTime dateCreation = LocalDateTime.now();
 
-    public Utilisateur(String unNom, String unPrenom, int id, String unEmail)
+    public Utilisateur(String unNom, String unPrenom, int identifiant, String unEmail, String unMdp, Role unRole)
     {
+        this.identifiant = identifiant;
         this.nom = unNom;
         this.prenom = unPrenom;
-        this.identifiant = id;
         this.email = unEmail;
+        this.motDePasse = unMdp;
+        this.role = unRole;
+    }
+
+    public Utilisateur(String unNom, String unPrenom, int identifiant, String unEmail, String unMdp)
+    {
+        this.identifiant = identifiant;
+        this.nom = unNom;
+        this.prenom = unPrenom;
+        this.email = unEmail;
+        this.motDePasse = unMdp;
     }
 
     public boolean connexion(int id, String mdp)
     {
-        if(this.identifiant == id && this.motDePasse == mdp)
+        if (this.identifiant == id && this.motDePasse == mdp)
         {
             return true;
         }
         else
         {
+            // a quoi correspond le "s", ou va t-il être utilisé ?
             String s = "identifiant ou mot de passe incorrect";
             return false;
         }
@@ -70,22 +92,18 @@ public class Utilisateur
         return this.motDePasse = mdp;
     }
 
-
-    public boolean modifierMdp()
-    {
-        if(setMdp(this.motDePasse) == this.motDePasse)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    // /!\ Cette methode ne fonctionne pas, elle est à revoir
+    // public boolean modifierMdp() {
+    // if (setMdp(this.motDePasse) == this.motDePasse) {
+    // return true;
+    // } else {
+    // return false;
+    // }
+    // }
 
     public String toString()
     {
-        return "L'utilisateur " + this.nom + this.prenom + " a pour identifiant " + this.identifiant + ". Son mail est " + this.email;
+        return "L'utilisateur " + this.nom + this.prenom + " a pour identifiant " + this.identifiant + ". Son mail est "
+                + this.email;
     }
 
-}
