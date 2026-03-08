49204ef324ad60e26ad4d896945a33ad2c24a1c4
Yasmine BENDJEBBOUR <yasminebendjebbour@gmail.com>
2026-03-08 17:31:11 +0100

    Test de modele

--- src/main/java/future/SAE/domain/model/Eleve.java
+++ src/main/java/future/SAE/domain/model/Eleve.java
@@ -4,4 +4,10 @@ import future.SAE.domain.valueObject.Role;
 
 public class Eleve extends Utilisateur
 {
+    private Formation formation;
 
+    // constructeur
+    public Eleve(String unNom, String unPrenom, int identifiant, String unEmail, String unMdp)
+    {
+        super(unNom, unPrenom, identifiant, unEmail, unMdp, Role.ELEVE);
+    }
