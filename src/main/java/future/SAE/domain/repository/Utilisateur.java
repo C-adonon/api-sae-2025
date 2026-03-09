37b51108736d79d0fd40cadc472d33f7113a42ec
Yasmine BENDJEBBOUR <yasminebendjebbour@gmail.com>
2026-03-09 21:55:17 +0100

    DTO

---
+++ src/main/java/future/SAE/domain/repository/Utilisateur.java
@@ -0,0 +1,12 @@
+package future.SAE.domain.repository;
+import java.util.Optional;
+import future.SAE.domain.model.Utilisateur;
+
+public interface Utilisateur
+{
+    Utilisateur save(Utilisateur utilisateur);
+    Optional<Utilisateur> findById(int id);
+    Utilisateur update(Utilisateur utilisateur);
+    Utilisateur findByEmail(String email);
+    void deleteById(int id);
+}
