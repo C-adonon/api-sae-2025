4d31329c34c32e6bc3545719db966d13d47c44a9
ikhd95 <issam.khadrouf@edu.univ-paris13.fr>
2026-03-09 14:52:52 +0100

    correction du model Formation

--- src/main/java/future/SAE/domain/model/Formation.java
+++ src/main/java/future/SAE/domain/model/Formation.java
@@ -1,59 +1,39 @@
+package future.SAE.domain.model;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import future.SAE.domain.valueObject.Semestre;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Formation {
 
-    private int annee;
+    private Long idFormation;
     private String nom;
-    private List<Professeur> responsables;
+    private int annee;
     private Semestre semestre;
-    private List<Cours> cours;
-    private List<Competences> competences;
+    private Professeur responsable;
+    private List<Competence> competences = new ArrayList<>();
+    private LocalDateTime dateCreation = LocalDateTime.now();
+    private LocalDateTime dateModification;
+}
 
-    public Formation(int annee, String nom, List<Professeur> responsables, Semestre semestre) {
+    public Formation() {
+
+    }
+
+    public Formation(int annee, String nom, Professeur unResponsable, Semestre semestre) {
         this.annee = annee;
         this.nom = nom;
         this.semestre = semestre;
-        this.cours = new ArrayList<>();
-        this.competences = new ArrayList<>();
-        setResponsables(responsables);
+        this.responsable = unResponsable;
     }
 
-    // getters
-    public int getAnnee() {
-        return annee;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public List<Professeur> getResponsables() {
-        return responsables;
-    }
-
-    public Semestre getSemestre() {
-        return semestre;
-    }
-
-    public void setAnnee(int annee) {
-        this.annee = annee;
-    }
-
-    // setters
-    public void setSemestre(Semestre semestre) {
-        this.semestre = semestre;
-    }
-
-    public void setResponsables(List<Professeur> responsables) {
-        if (responsables == null || responsables.size() < 1 || responsables.size() > 2) {
-            throw new IllegalArgumentException("Une formation doit avoir entre 1 et 2 responsables.");
-        }
-        this.responsables = responsables;
-    }
-
-    @Override
-    public String toString() {
-        return "Formation: " + nom + " (" + semestre + ")";
-    }
+@Override
+public String toString(){
+    return "Formation: " + this.nom + " (" + this.semestre + ")";
 }
