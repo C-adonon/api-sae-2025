d29b0dfbb5063b75d1c570c40addf98b0556a77d
Yasmine BENDJEBBOUR <yasminebendjebbour@gmail.com>
2026-03-09 20:45:19 +0100

    DTO

--- src/main/java/future/SAE/domain/model/Utilisateur.java
+++ src/main/java/future/SAE/domain/model/Utilisateur.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import future.SAE.domain.valueObject.Role;
 import lombok.Getter;
 import lombok.Getter;
+
 @Getter
 @Setter
 public abstract class Utilisateur
@@ -18,11 +19,12 @@ public abstract class Utilisateur
     private Role role;
     private LocalDateTime dateCreation = LocalDateTime.now();
 
+    //constructeur vide
     public Utilisateur()
     {
 
     }
-
+    //constructeur d'un utilisateur avec un role: professeur ou eleve
     public Utilisateur(String unNom, String unPrenom, int unIdentifiant, String unEmail, String unMdp, Role unRole)
     {
         this.nom = unNom;
@@ -33,6 +35,7 @@ public abstract class Utilisateur
         this.role = unRole;
     }
 
+    //constructeur d'un utilisateur sans role
     public Utilisateur(String unNom, String unPrenom, int unIdentifiant, String unEmail, String unMdp)
     {
         this.nom = unNom;
@@ -41,7 +44,7 @@ public abstract class Utilisateur
         this.email = unEmail;
         this.motDePasse = unMdp;
     }
-
+    // affichage de l'utilisateur lors des test
     public String toString()
     {
         return "L'utilisateur " + this.nom + this.prenom + " a pour identifiant " + this.identifiant + ". Son mail est " + this.email;
