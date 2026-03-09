fe7bbde64cb14155d7aca5bbc0d74a33a5319bb0
ikhd95 <issam.khadrouf@edu.univ-paris13.fr>
2026-03-09 14:43:51 +0100

    correction du model Fichier

--- src/main/java/future/SAE/domain/model/Fichier.java
+++ src/main/java/future/SAE/domain/model/Fichier.java
@@ -1,62 +1,42 @@
+package future.SAE.domain.model;
+
 import java.time.LocalDateTime;
+import future.SAE.domain.valueObject.Type;
+import lombok.Getter;
+import lombok.Setter;
 
-public abstract class Fichier {
-
+@Getter
+@Setter
+public class Fichier {
+    private Long idFichier;
     private String titre;
     private String description;
     private String cheminFichier;
-    private LocalDateTime datePublication;
     private Type type;
+    private Section section;
+    private LocalDateTime datePublication = LocalDateTime.now();
 
-    public Fichier(String titre, String cheminFichier, Type type) {
+    public Fichier() {
+    }
+
+    public Fichier(String titre, String cheminFichier, Section uneSection, Type type) {
         this.titre = titre;
         this.cheminFichier = cheminFichier;
+        this.section = uneSection;
         this.type = type;
-        this.datePublication = LocalDateTime.now();
     }
 
-    public Fichier(String titre, String description, String cheminFichier, Type type) {
+    public Fichier(String titre, String uneDescription, String cheminFichier, Section uneSection, Type type) {
         this.titre = titre;
         this.cheminFichier = cheminFichier;
+        this.section = uneSection;
         this.type = type;
-        this.datePublication = LocalDateTime.now();
-        this.description = description;
-    }
-
-    public String getTitre() {
-        return titre;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getCheminFichier() {
-        return cheminFichier;
-    }
-
-    public LocalDateTime getDatePublication() {
-        return datePublication;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setTitre(String titre) {
-        this.titre = titre;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setCheminFichier(String chemin) {
-        this.cheminFichier = chemin;
+        this.description = uneDescription;
     }
 
     @Override
     public String toString() {
-        return "Fichier : " + titre + " [" + type + "]";
+        return "Fichier: " + this.titre + "." + this.type;
     }
+
 }
