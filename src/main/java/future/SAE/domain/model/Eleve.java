50fade031991a892c4932e5a1318390ce427eebf
Yasmine BENDJEBBOUR <yasminebendjebbour@gmail.com>
2026-03-09 21:25:53 +0100

    DTO

--- src/main/java/future/SAE/domain/model/Eleve.java
+++ src/main/java/future/SAE/domain/model/Eleve.java
@@ -10,4 +10,31 @@ import lombok.Setter;
 @Setter
 public class Eleve
 {
+    private Formation formation;
+    private List<InscriptionCours> inscriptions = new ArrayList<>();
+
+    //constructeur avec heritage
+    public Eleve()
+    {
+        super();
+    }
+
+    //constructeur d'un utilisateur ayant pour role ELEVE
+    public Eleve(String unNom, String unPrenom, int unIdentifiant, String unEmail, String unMdp)
+    {
+        super(unNom, unPrenom, unIdentifiant, unEmail, unMdp, Role.ELEVE);
+    }
+
+    //constructeur d'un utilisateur ayant pour role ELEVE qui est relie a une formation et une liste
+    public Eleve(String unNom, String unPrenom, int unIdentifiant, String unEmail, String unMdp, Formation uneFormation, List<InscriptionCours> listeInscriptionCours)
+    {
+        super(unNom, unPrenom, unIdentifiant, unEmail, unMdp, Role.ELEVE);
+        this.formation = uneFormation;
+        this.inscriptions = listeInscriptionCours;
+    }
+
+    public String toString()
+    {
+        return "Eleve " + super.toString() + " fait partie de la formation " + this.formation + " et de la liste " + this.inscriptions;
+    }
 }
