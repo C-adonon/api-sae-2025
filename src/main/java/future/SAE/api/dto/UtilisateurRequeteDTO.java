c984384c88f5f1baa450e84aa4021cb105614dfb
Yasmine BENDJEBBOUR <yasminebendjebbour@gmail.com>
2026-03-10 21:15:25 +0100

    DTO

--- src/main/java/future/SAE/api/dto/UtilisateurRequeteDTO.java
+++ src/main/java/future/SAE/api/dto/UtilisateurRequeteDTO.java
@@ -1,19 +1,5 @@
 package future.SAE.api.dto;
-import java.util.UUID;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 
-public class UtilisateurRequeteDTO
-{
-    private int identifiant;
-    private String nom;
-    private String prenom;
-    private String email;
-    private String motDePasse;
-    private Role role;
-    private UUID formationId;
+public class UtilisateurRequeteDTO {
 }
-
