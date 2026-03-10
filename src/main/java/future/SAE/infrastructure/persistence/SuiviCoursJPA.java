abb72c264508e72a050ea49c83a153f812bac46a
Clarence Edoh-Dagnon <clarenceedohdagnon@gmail.com>
2026-03-10 11:31:30 +0100

    Création de la classe SuiviCoursJPA.java

---
+++ src/main/java/future/SAE/infrastructure/persistence/SuiviCoursJPA.java
@@ -0,0 +1,39 @@
+package future.SAE.infrastructure.persistence;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.Setter;
+import lombok.Getter;
+
+@Entity
+@Table(name = "suivi_cours", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"id_cours", "id_eleve"})
+})
+@Getter
+@Setter
+public class SuiviCoursJPA {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_suivi_cours", nullable = false)
+    private Long idSuiviCours;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_cours")
+    private CoursJPA cours;
+
+    @ManyToOne(fetch =  FetchType.LAZY)
+    @JoinColumn(name = "id_eleve", nullable = false)
+    private EleveJPA eleve;
+
+    @Column(name = "progression_globale")
+    private float progressionGlobale;
+
+    @Column(name = "date_completion")
+    private LocalDateTime dateCompletion = LocalDateTime.now;
+
+    public SuiviCoursJPA(){
+    }
+
+}
+
