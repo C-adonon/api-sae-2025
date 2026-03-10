da2f3f0c3ec10e631501bac39edd1338af77b071
Yasmine BENDJEBBOUR <yasminebendjebbour@gmail.com>
2026-03-10 16:41:29 +0100

    DTO

--- src/test/java/TestEleve.java
+++ src/test/java/TestEleve.java
@@ -1,12 +1,28 @@
 import future.SAE.domain.model.Eleve;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class TestEleve
 {
     @Test
     public void TestEleve()
     {
-        Eleve e = new Eleve("BENDJEBBOUR", "Yasmine", 12301458, "yasmine.bendjebbour@edu.univ.fr");
+        Eleve e = new Eleve("BENDJEBBOUR", "Yasmine", 12301458, "yasmine.bendjebbour@edu.univ.fr", "Y@smine2005");
         System.out.println(e.toString());
+
+        Formation f = new Formation("informatique");
+
+        List<InscriptionCours> listeInscriptions = new ArrayList<>();
+        InscriptionCours i = new InscriptionCours("bd");
+        InscriptionCours i1 = new InscriptionCours("flask");
+
+        listeInscriptions.add(i);
+        listeInscriptions.add(i1);
+
+        Eleve e1 = new Eleve("ADONON", "Chloe", 12401659, "chloe.adonon@edu.univ.fr", "Chl0e2000", f, listeInscriptions );
+
+        System.out.println(e);
+        System.out.println(e1);
     }
 }
