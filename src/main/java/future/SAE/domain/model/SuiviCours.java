2da734e0bbc484a3d9c8e6333edbf56ec8647098
Clarence Edoh-Dagnon <clarenceedohdagnon@gmail.com>
2026-03-10 01:01:22 +0100

    Modification de la classe SuiviCours.java

--- src/main/java/future/SAE/domain/model/SuiviCours.java
+++ src/main/java/future/SAE/domain/model/SuiviCours.java
@@ -1,53 +1,30 @@
 package future.SAE.domain.model;
+
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Getter
+@Setter
 public class SuiviCours {
 
 
-    private Long id;
+    private Long idSuiviCours;
     private float progressionGlobale;
     private LocalDateTime dateDernierAcces;
-
-
     private Eleve eleve;
+
 
     public SuiviCours(Eleve unEleve){
         this.eleve = unEleve;
     }
 
-    public float getProgressionGlobale() {
-        return progressionGlobale;
-    }
-
-    public Eleve getEleve() {
-        return eleve;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public LocalDateTime getDateDernierAcces() {
-        return dateDernierAcces;
-    }
-
-    public void setProgressionGlobale(float progressionGlobale) {
-        this.progressionGlobale = progressionGlobale;
-    }
-
-    public void setDateDernierAcces(LocalDateTime dateDernierAcces) {
-        this.dateDernierAcces = dateDernierAcces;
-    }
-
-    public void setEleve(Eleve eleve) {
-        this.eleve = eleve;
-    }
 
 
     public String toString() {
-        // à faire
-        return "";
+
+        return "Dernier accès au cours : " + this.getDateDernierAcces() + "\n" +
+                "Progression Globale : " + this.getProgressionGlobale() + " %";
     }
 }
