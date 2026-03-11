2e93c83c1ec0e99701b42d4013ed9a60c293e57c
Clarence Edoh-Dagnon <clarenceedohdagnon@gmail.com>
2026-03-11 14:56:18 +0100

    Création de la classe TestCompetence.java

---
+++ src/test/java/TestCompetence.java
@@ -0,0 +1,48 @@
+import future.SAE.domain.model.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestCompetence {
+
+    @Test
+    @DisplayName("Test du constructeur (avec description) du model \"Competence.java\"")
+    void testConstructeurAvecDescription(){
+
+        Formation formation = new Formation();
+        int numero = 1;
+        String libelle = "Développement d'application";
+        String description = "API + client responsive";
+
+        Competence competence = new Competence(numero,libelle,formation,description);
+
+        assertAll("Vérification des attributs de la compétence",
+                () -> assertEquals(numero, competence.getNumero()),
+                () -> assertEquals(libelle, competence.getLibelle()),
+                () -> assertEquals(description, competence.getDescription()),
+                () -> assertEquals(formation, competence.getFormation())
+        );
+    }
+
+    @Test
+    @DisplayName("Test du constructeur sans description du model \"Competence.java\"")
+    void testConstructeurSansDescription(){
+        Formation formation = new Formation();
+
+        Competence competence = new Competence(5,"Optimisation",formation);
+
+        assertNull(competence.getDescription(), "La description devrait être null");
+
+    }
+
+    @Test
+    @DisplayName("Test de la méthode toString() du model \"Competence.java\"")
+    void testToString(){
+        Competence competence = new Competence(3, "Administration de systèmes", null, "Conteneurisation");
+
+        String attendu = "Compétence C3\nLibellé : Administration de systèmes\nDescription : Conteneurisation\n";
+    }
+
+
+}
