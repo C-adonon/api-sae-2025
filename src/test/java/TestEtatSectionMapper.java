761beb236e2617cc3959f0afd9c2c3c6804fa270
Clarence Edoh-Dagnon <clarenceedohdagnon@gmail.com>
2026-03-11 19:35:16 +0100

    Création de la classe TestEtatSectionMapper.java

---
+++ src/test/java/TestEtatSectionMapper.java
@@ -0,0 +1,35 @@
+import future.SAE.infrastructure.mapping.EtatSectionMapper;
+import future.SAE.domain.valueObject.EtatSection;
+import future.SAE.infrastructure.persistence.EleveJPA;
+import future.SAE.infrastructure.persistence.EtatSectionJPA;
+import future.SAE.infrastructure.persistence.SectionJPA;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class TestEtatSectionMapper {
+
+    @Autowired
+    EtatSectionMapper etatSectionMapper;
+
+    @Test
+    @DisplayName("Devrait mapper correctement une Competence vers la persistence")
+    void testToEntity(){
+
+        EtatSection domain = new EtatSection();
+        domain.setIdEtatSection(1);
+        domain.setDateCompletion(LocalDateTime.now());
+
+        etatSectionMapper.toEntity(domain);
+
+        assert
+    }
+
+}
