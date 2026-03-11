0e5096f09e067e3914d782c11d63492b9755337f
Yasmine BENDJEBBOUR <yasminebendjebbour@gmail.com>
2026-03-11 12:49:05 +0100

    DTO

---
+++ src/main/java/future/SAE/api/dto/FichierReponseDTO.java
@@ -0,0 +1,17 @@
+package future.SAE.api.dto;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FichierReponseDTO
+{
+    private Long id;
+    private String titre;
+    private String description;
+    private String cheminFichier;
+    private Type type;
+    private Long sectionId;
+    private LocalDateTime datePublication;
+}
