0c8b4b541c9b3bde88b728b4b0c942e9d54cacf8
Clarence Edoh-Dagnon <clarenceedohdagnon@gmail.com>
2026-03-12 16:00:23 +0100

    init TestCompetenceService.java

--- src/test/java/TestCompetenceService.java
+++ src/test/java/TestCompetenceService.java
@@ -1,5 +1,6 @@
 import future.SAE.application.service.CompetenceService;
 import future.SAE.domain.model.Competence;
+import future.SAE.infrastructure.mapping.CompetenceMapper;
 import future.SAE.infrastructure.persistence.CompetenceJPA;
 import future.SAE.infrastructure.repository.CompetenceRepository;
 import org.junit.jupiter.api.Test;
@@ -11,8 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TestCompetenceService {
@@ -20,20 +20,42 @@ public class TestCompetenceService {
     @Mock
     private CompetenceRepository competenceRepository;
 
+    @Mock
+    private CompetenceMapper competenceMapper;
+
     @InjectMocks
     private CompetenceService competenceService;
 
     @Test
-    void testTrouverParId(){
+    void testFindById(){
 
         CompetenceJPA jpa = new CompetenceJPA();
         jpa.setIdCompetence(1L);
-        when(competenceRepository.findById(1L)).thenReturn(Optional.of(jpa));
+        when(this.competenceRepository.findById(1L)).thenReturn(Optional.of(jpa));
 
-        Competence result = competenceService.getCompetenceById(1L);
+        Competence result = this.competenceService.getCompetenceById(1L);
 
         assertNotNull(result);
-        verify(competenceRepository).findById(1L);
+        verify(this.competenceRepository).findById(1L);
 
     }
+
+    @Test
+    void testCreerCompetence() {
+
+        Competence competence = new Competence();
+        CompetenceJPA jpa = new CompetenceJPA();
+
+        when(this.competenceMapper.toEntity(competence)).thenReturn(jpa);
+        when(this.competenceRepository.save(jpa)).thenReturn(jpa);
+        when(this.competenceMapper.toDomain(jpa)).thenReturn(competence);
+
+        Competence result = this.competenceService.createCompetence(competence);
+
+        assertNotNull(result);
+        verify(this.competenceRepository).save(jpa);
+
+    }
+
+
 }
