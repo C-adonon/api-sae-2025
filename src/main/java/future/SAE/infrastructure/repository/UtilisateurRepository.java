a619e0ecf3413d970a615c507d56b8f2072aaea8
yasmine <yasminebendjebbour@gmail.com>
2026-03-12 18:07:39 +0100

    Repository

--- src/main/java/future/SAE/infrastructure/repository/UtilisateurRepository.java
+++ src/main/java/future/SAE/infrastructure/repository/UtilisateurRepository.java
@@ -1,1 +1,45 @@
+package future.SAE.infrastructure.repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import future.SAE.infrastructure.persistence.EleveJPA;
+import future.SAE.infrastructure.persistence.UtilisateurJPA;
+
+@Repository
+public interface UtilisateurRepository extends JpaRepository<UtilisateurJPA, UUID>
+{
+    Optional<UtilisateurJPA> findByEmail(String email);
+    Optional<UtilisateurJPA> findByIdentifiant(Long identifiant);
+
+    //Les eleves d'une formation
+    List<EleveJPA> findAllElevesByFormationIdFormation(Long idFormation);
+
+    //Les eleves d'un cours dans l'ordre alphabetique
+    List<EleveJPA> findAllElevesJPAByInscriptionsCoursIdCoursOrdersByNomAsc(Long idCours);
+
+    //Les eleves ayant fini le cours dans l'ordre alphabetique
+    @Query("SELECT suivi.eleve FROM SuiviCoursJPA suivi WHERE suivi.cours.idCours = :idCours AND suivi.progressionGlobale = 100")
+    List<EleveJPA> findElevesCoursTermine(Long idCours);
+
+    //Les eleves n'ayant pas fini le cours dans l'ordre alphabetique
+    @Query("SELECT suivi.eleve FROM SuiviCoursJPA suivi WHERE suivi.cours.idCours = :idCours AND suivi.progressionGlobale = 100")
+    List<EleveJPA> findElevesCoursNonTermine(Long idCours);
+
+    //verifie si un professeur existe par son mail et son role
+    boolean existsByEmailAndRole(String email, String role);
+
+    @Query("SELECT p FROM ProfesseurJPA p");
+    List<ProfesseurJPA> findAllProfesseur();
+
+    Optional<ProfesseurJPA> findByIdentifiantAndRole(Long identifiant, String role);
+
+    @Query("SELECT e FROM EleveJPA e");
+    List<EleveJPA> findAllEleve();
+
+
+}
