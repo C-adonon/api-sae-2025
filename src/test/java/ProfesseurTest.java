abd41fcc59662eabff7dc43525c25c7740610b3a
Clarence Edoh-Dagnon <clarenceedohdagnon@gmail.com>
2026-06-09 01:42:07 +0200

    refactor: change Utilisateur class to abstract and add ProfesseurTest class with attribute tests

---
+++ src/test/java/ProfesseurTest.java
@@ -0,0 +1,38 @@
+import future.SAE.domain.model.Professeur;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+
+public class ProfesseurTest
+{
+
+    String nom,prenom,email,mdp;
+    int identifiant;
+    Professeur p;
+
+    @BeforeEach
+    public void initProfesseur()
+    {
+        nom = "Edoh-Dagnon";
+        prenom = "Clarence";
+        identifiant = 1 ;
+        email = "clarence@example.com";
+        mdp = "motdepasse";
+        p = new Professeur(nom, prenom, identifiant, email, mdp);
+    }
+
+    @Test
+    public void testAttributs(){
+        assertEquals(p.getNom(),nom);
+        assertEquals(p.getPrenom(), prenom);
+        assertEquals(p.getEmail(), email);
+        assertEquals(p.getMotDePasse(), mdp);
+        assertEquals(p.getIdentifiant(), identifiant);
+    }
+}
