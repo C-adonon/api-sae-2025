80bd5c605ab84055dc13fec7b349a3679da765f3
yasmine <yasminebendjebbour@gmail.com>
2026-06-15 15:45:39 +0200

    testEleve

--- src/test/java/domain/model/TestEleve.java
+++ src/test/java/domain/model/TestEleve.java
@@ -1,4 +1,3 @@
-package domain.model;
 import future.SAE.domain.model.Eleve;
 import org.junit.jupiter.api.Test;
 
@@ -7,8 +6,26 @@ import java.util.ArrayList;
 public class TestEleve
 {
     @Test
-    public void TestEleve()
+    public void creerEleve()
     {
+        Eleve e = new Eleve("BENDJEBBOUR", "Yasmine", 12301458, "yasmine.bendjebbour@edu.univ.fr", "Y@smine2005!");
+        Assert.isTrue(e.getNom().equals("BENDJEBBOUR"), "Son nom est Bendjebbour");
+        Assert.isTrue(e.getPrenom().equals("Yasmine"), "Son prénom est Yasmine");
+        Assert.isTrue(e.getIdentifiant() == 12301458), "Son id est 12301458");
+        Assert.isTrue(e.getEmail().equals("yasmine.bendjebbour@edu.univ.fr"), "Adresse mail valide");
+    }
 
+    @Test
+    public void iscritEleve()
+    {
+        //On initialise une formation et une liste d'inscription
+        Formation f = new Formation("Informatique");
+        List<InscriptionCours> maListe = new ArrayList<>();
+
+        //On initialise un eleve avec la formation et la liste d'inscription créée
+        Eleve v = new Eleve("ADONON", "Chloe", 12512557, "chloe.adonon@edu.univ.fr", "Chl0e2000!", f, mesInscriptions);
+
+        Assert.isTrue(v.getFormation().equals(f), "L'élève est en informatique");
+        Assert.isTrue(e.getInscriptions().equals(maListe), "L'élève fais parti de la liste d'inscription");
     }
 }
