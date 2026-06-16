deb0152fa8c398adac9871005c02ce137a344976
yasmine <yasminebendjebbour@gmail.com>
2026-06-16 16:12:22 +0200

    Test cours

--- src/test/java/domain/model/coursTest.java
+++ src/test/java/domain/model/coursTest.java
@@ -4,8 +4,9 @@ public class coursTest
     public void creerCours()
     {
         Professeur p = new Professeur("EDOH-DAGNON", "Clarence", "14503586", "clarence.edohdagnon@edu.univ.fr","Cl@rence2006!");
-        Formation f = new Formation()
-        Cours c = new Cours("Reseau",  )
+        Formation f = new Formation(1, "Informatique", p, Semestre.S1);
+        Cours c = new Cours("Reseau", p, f);
+        Assert.isTrue()
     }
 
 }
