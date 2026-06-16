f2937e2353ee9f888e7f260baceaba1711aae689
yasmine <yasminebendjebbour@gmail.com>
2026-06-16 17:02:55 +0200

    Test cours

--- src/test/java/domain/model/coursTest.java
+++ src/test/java/domain/model/coursTest.java
@@ -2,17 +2,24 @@ package future.SAE.domain.model;
 
 public class coursTest
 {
+    private Professeur p;
+    private Formation f;
+    private Cours c;
+
+    @BeforeEach
+    public void init()
+    {
+        p = new Professeur("EDOH-DAGNON", "Clarence", "14503586", "clarence.edohdagnon@edu.univ.fr","Cl@rence2006!");
+        f = new Formation(1, "Informatique", p, Semestre.S1);
+        c = new Cours("Reseau", p, f);
+    }
     @Test
     public void creerCours()
     {
-        Professeur p = new Professeur("EDOH-DAGNON", "Clarence", "14503586", "clarence.edohdagnon@edu.univ.fr","Cl@rence2006!");
-        Formation f = new Formation(1, "Informatique", p, Semestre.S1);
-        Cours c = new Cours("Reseau", p, f);
         Assert.isTrue(c.getNom.equals("Reseau"), "C'est un cours de réseau");
         Assert.isTrue(c.getResponsable.equals(p), "Le responsable du cours est Clarence");
         Assert.isTrue(c.getFormation.equals("Reseau"), "C'est un cours de la formation informatique");
     }
-
     @Test
 
 
