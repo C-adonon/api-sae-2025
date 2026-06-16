23b2b7b05884d73c24bf8f8ede6d2af0c8e57a43
yasmine <yasminebendjebbour@gmail.com>
2026-06-16 16:16:44 +0200

    Test cours

--- src/test/java/domain/model/coursTest.java
+++ src/test/java/domain/model/coursTest.java
@@ -6,7 +6,9 @@ public class coursTest
         Professeur p = new Professeur("EDOH-DAGNON", "Clarence", "14503586", "clarence.edohdagnon@edu.univ.fr","Cl@rence2006!");
         Formation f = new Formation(1, "Informatique", p, Semestre.S1);
         Cours c = new Cours("Reseau", p, f);
-        Assert.isTrue()
+        Assert.isTrue(c.getNom.equals("Reseau"), "C'est un cours de réseau");
+        Assert.isTrue(c.getResponsable.equals(p), "Le responsable du cours est Clarence");
+        Assert.isTrue(c.getFormation.equals("Reseau"), "C'est un cours de la formation informatique");
     }
 
 }
